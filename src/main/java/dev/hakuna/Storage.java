3e66a6cb3fa45df4179637547081664bdea44845
DexTher22 <markmagyar2003@gmail.com>
2026-03-31 15:33:24 +0200

    kereső működik

--- src/main/java/dev/hakuna/Storage.java
+++ src/main/java/dev/hakuna/Storage.java
@@ -3,7 +3,7 @@ package dev.hakuna;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
 
 public class Storage {
